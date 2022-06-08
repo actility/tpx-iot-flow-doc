3b28f065fbee394be3e9e7ffd3a4cf27db964d5b
Nathaniael TRISTRAM <nathaniael.tristram@epitech.eu>
2022-06-08 13:06:54 +0200

    Add Advantech connector

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -351,6 +351,12 @@ module.exports = {
                         collapsable: false, // optional, defaults to true
                         sidebarDepth: 1,    // optional, defaults to 1
                     },
+                    {
+                        title: 'Connecting to ADVANTECH',   // required
+                        path: '/Connector/ADVANTECH/',      // optional, link of the title, which should be an absolute path and must exist
+                        collapsable: false, // optional, defaults to true
+                        sidebarDepth: 1,    // optional, defaults to 1
+                    },
                     ]
                 },
                 {
