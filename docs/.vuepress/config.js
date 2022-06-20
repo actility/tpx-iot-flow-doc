73eff1f70ed463d52c40ddeb256477d0efc81cd5
Bruno Régnier <bruno.regnier@actility.com>
2022-06-20 18:27:19 +0200

    Comment unexpected connector on 1.6.x

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -286,6 +286,12 @@ module.exports = {
                             sidebarDepth: 1,    // optional, defaults to 1
                         },
                         {
+                            title: 'Connecting to ADVANTECH',   // required
+                            path: '/Connector/ADVANTECH/',      // optional, link of the title, which should be an absolute path and must exist
+                            collapsable: false, // optional, defaults to true
+                            sidebarDepth: 1,    // optional, defaults to 1
+                        },
+                        /* {
                             title: 'Connecting to MODBUS',   // required
                             path: '/Connector/MODBUS/',      // optional, link of the title, which should be an absolute path and must exist
                             collapsable: false, // optional, defaults to true
@@ -351,12 +357,7 @@ module.exports = {
                         collapsable: false, // optional, defaults to true
                         sidebarDepth: 1,    // optional, defaults to 1
                     },
-                    {
-                        title: 'Connecting to ADVANTECH',   // required
-                        path: '/Connector/ADVANTECH/',      // optional, link of the title, which should be an absolute path and must exist
-                        collapsable: false, // optional, defaults to true
-                        sidebarDepth: 1,    // optional, defaults to 1
-                    },
+                    */
                     ]
                 },
                 {
